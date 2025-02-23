bf92100cb734447da5e5a2199c24fb4f5eb62f53
JoshuaRosado <joshua06rosado@gmail.com>
2025-02-23 07:32:31 -0600

    Initial Commit

---
+++ Bookworm/BookwormApp.swift
@@ -0,0 +1,17 @@
+//
+//  BookwormApp.swift
+//  Bookworm
+//
+//  Created by Joshua Rosado Olivencia on 2/23/25.
+//
+
+import SwiftUI
+
+@main
+struct BookwormApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ BookwormTests/BookwormTests.swift
@@ -0,0 +1,17 @@
+//
+//  BookwormTests.swift
+//  BookwormTests
+//
+//  Created by Joshua Rosado Olivencia on 2/23/25.
+//
+
+import Testing
+@testable import Bookworm
+
+struct BookwormTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
