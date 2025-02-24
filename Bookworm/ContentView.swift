1f3564ad1c3df5506fb944b37c5401474083e97f
JoshuaRosado <joshua06rosado@gmail.com>
2025-02-24 11:19:25 -0600

    Difference between textEditor and textField

--- Bookworm/ContentView.swift
+++ Bookworm/ContentView.swift
@@ -11,49 +11,27 @@
 //@Bindable = Bindings to properties inside observable classes
 
 
-//@Binding = Allows us to share an @State that belong to one View with another View
+//@Binding = Allows us to share an @State that belongs to one View with another View
+
+
+// @AppStorage = Is good to store simple things
+// IS NOT SECURE to store private information (username,password,etc)
 
 import SwiftUI
 
-struct PushButton: View {
-    let title: String
-    @Binding var isOn: Bool
-    
-    var onColors = [Color.red, Color.yellow]
-    var offColors = [Color(white:0.6), Color(white: 0.4)]
-    
-    var body: some View {
-        Button(title) {
-            isOn.toggle()
-        }
-        .padding()
-        // If Bool (isOn) is true
-        // Display onColors
-        // If (isOn) is false
-        // Display offColors
-        .background(LinearGradient(colors: isOn ? onColors : offColors, startPoint: .top, endPoint: .bottom))
-        .foregroundStyle(.white)
-        .clipShape(.capsule)
-        .shadow(radius: isOn ? 0 : 5)
-        
-    }
-}
-
 struct ContentView: View {
     
-    @State private var rememberMe = false
+    @AppStorage("notes") private var notes = ""
     
     var body: some View {
-        VStack {
-            
-            PushButton(title: "Remember Me", isOn: $rememberMe)
-            // Using rememberMe with "$" because this parameters is used with @Binding
-            
-            
-            Text(rememberMe ? "On" : "Off")
+        NavigationStack {
+            // Using axis will help to expand the TextField to be displayed in multiple lines
+            TextField("Enter your text", text: $notes, axis: .vertical)
+                .textFieldStyle(.roundedBorder)
+                .navigationTitle("Notes")
+                .padding()
             
         }
-        
     }
 }
 
